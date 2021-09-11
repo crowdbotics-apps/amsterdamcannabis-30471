3beef858d1dca88716491a8eda66bc61238e2db9
Crowdbotics <team@crowdbotics.com>
2021-09-11 18:02:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "amsterdamcannabis_30471Identifier",
+  url: "https://amsterdamcannabis_30471Identifier.botics.co",
+  api: "https://amsterdamcannabis_30471Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
