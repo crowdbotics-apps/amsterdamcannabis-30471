d50ffb57936d1f02334d1a19c1af15b0345f8d68
Crowdbotics <team@crowdbotics.com>
2021-09-11 18:04:26 +0000

    Update models

--- backend/amsterdamcannabis_30471/wsgi.py
+++ backend/amsterdamcannabis_30471/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amsterdamcannabis_30471.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amsterdamcannabis_30471.settings")
 
 application = get_wsgi_application()
